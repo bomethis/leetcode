f5b126a20e3b19d470b721d5ac902ddeb0d69489
Bebo <joelbebo@gmail.com>
2019-06-06 15:29:23 -0400

    my submission

---
+++ unique_morse_code_words.py
@@ -0,0 +1,24 @@
+class Solution:
+    def uniqueMorseRepresentations(words):
+        dicts = {"a": ".-", "b": "-...", 'c': "-.-.", 'd': "-..", 'e': ".", 'f': "..-.", 'g': "--.", 'h': "....", 'i': "..", 'j': ".---", 'k': "-.-", 'l': ".-..", 'm': "--", 'n': "-.", 'o': "---", 'p': ".--.", 'q': "--.-", 'r': ".-.", 's': "...", 't': "-", 'u': "..-", 'v': "...-", 'w': ".--", 'x': "-..-", 'y': "-.--", 'z': "--.."}
+        i = 0
+        lwords =[]
+        for word in words:
+            lword = list(word)  # convert imutible str into a list in order to replace letters in each index
+            for i in range(len(word)):
+                lword[i] = dicts[word[i]]  # replace each letter with corresponding morse code
+            jword = ''.join(lword)  # convert the morse code back into a str
+            lwords.append(jword)
+        return len(set(lwords))  # return the count of unique transformations
+
+
+words = ["gin", "zen", "gig", "msg"]
+
+print(Solution.uniqueMorseRepresentations(words))
+
+
+
+# Success
+# Details
+# Runtime: 40 ms, faster than 71.72% of Python3 online submissions for Unique Morse Code Words.
+# Memory Usage: 13.2 MB, less than 43.43% of Python3 online submissions for Unique Morse Code Words.
