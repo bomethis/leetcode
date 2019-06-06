b8f37ce008be79c06979fa4448a4c58e759f581a
Bebo <joelbebo@gmail.com>
2019-06-06 12:40:13 -0400

    my submission

---
+++ to_lower_case.py
@@ -0,0 +1,11 @@
+
+class Solution:
+    def toLowerCase(self, str):
+        return str.lower()  # return lower case of str argument
+
+
+# Success
+# Details
+# Runtime: 28 ms, faster than 98.62% of Python3 online submissions for To Lower Case.
+# Memory Usage: 13.2 MB, less than 42.05% of Python3 online submissions for To Lower Case.
+# Next challenges:
